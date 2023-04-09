d525dc97ead3a8dc90551418f5dbfce7dab002e6
Aayush-kulkarni567 <104626152+Aayush-kulkarni567@users.noreply.github.com>
2023-04-09 13:33:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import {Login , Homepage, Register}  from './components/index'
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      {/* <Register /> */}
+      {/* <Homepage /> */}
+    </div>
+  );
+}
+
+export default App;
